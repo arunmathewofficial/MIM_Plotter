d7c6b7e8a78e599bebccf238474e02e9ad3ad4bb
mathew <a.mathew@iitg.ac.in>
2024-02-25 18:38:09 +0000

    updating non-adiabatic_flow_analysis.py

--- non-adiabatic_flow_analysis.py
+++ non-adiabatic_flow_analysis.py
@@ -34,38 +34,47 @@ high_den_overtime_file = args.high_density_overtime
 #plt.rc('lines', linewidth=1.5)
 
 
-# x data
-x_ldst = get_basic_data(low_den_subtime_file)['x']
-x_ldot = get_basic_data(low_den_overtime_file)['x']
-x_hdot = get_basic_data(high_den_overtime_file)['x']
+parsec = 3.086e+18 # cm
+# x data in parsec
+x_ldst = get_basic_data(low_den_subtime_file)['x']/parsec
+x_ldot = get_basic_data(low_den_overtime_file)['x']/parsec
+x_hdot = get_basic_data(high_den_overtime_file)['x']/parsec
+
+time_ldst = r'\rm t = ' + str(get_basic_data(low_den_subtime_file)['time'].round(3))
+time_ldot = r'\rm t = ' + str(get_basic_data(low_den_overtime_file)['time'].round(3))
+time_hdot = r'\rm t = ' + str(get_basic_data(high_den_overtime_file)['time'].round(3))
 
 # generate 5 panel figure
-fig, ax = plt.subplots(5, 1, figsize=(5, 8), gridspec_kw = {'wspace':0, 'hspace':0}, sharex=True)
+fig, ax = plt.subplots(5, 1, figsize=(5, 8))
 
 ### Panel - 1 Temperature profile
 #ax[0].set_xlabel(r'$\rm x \, (cm)$', fontsize=18)
-ax[0].set_ylabel(r'$\rm log \, T \, (K)$', fontsize=14, labelpad=10)
-ax[0].plot(x_ldst, np.log10(get_temperature(low_den_subtime_file)), label=r'\rm t = ??? sub s',
-           color='black', linewidth=1)
-ax[0].plot(x_ldot, np.log10(get_temperature(low_den_overtime_file)), label=r'\rm t = ??? over s',
-           color='black', linestyle='--', linewidth=1)
-ax[0].legend(frameon=False, fontsize=12)
+ax[0].set_ylabel(r'$\rm log \, T \, (K)$', fontsize=14, labelpad=16)
+ax[0].plot(x_ldst, np.log10(get_temperature(low_den_subtime_file)), label=time_ldst,
+           color='saddlebrown', linewidth=1)
+ax[0].plot(x_ldot, np.log10(get_temperature(low_den_overtime_file)), label=time_ldot,
+           color='darkgoldenrod', linestyle='--', linewidth=1)
+ax[0].legend(frameon=False, fontsize=12, loc=(0.5, 0.3))
+ax[0].set_xlim(0, 28)
 #ax[0].xaxis.set_minor_locator(AutoMinorLocator())
 #ax[0].yaxis.set_minor_locator(AutoMinorLocator())
 ax[0].tick_params(axis="both", direction="in", which="both", bottom=True, top=True, left=True, right=True, length=2)
 
 ### Panel - 2 density profile
 #ax[1].set_xlabel(r'$\rm x \, (cm)$', fontsize=18)
-ax[1].set_ylabel(r'$\rm log \, \rho \, (g/cm^3)$', fontsize=14, labelpad=20)
+ax[1].set_ylabel(r'$\rm log \, \rho \, (g/cm^3)$', fontsize=14, labelpad=2)
 ax[1].plot(x_ldst, get_density(low_den_subtime_file),
-           label=r'\rm t = ??? sub s', color='black', linewidth=1)
+           label=time_ldst, color='saddlebrown', linewidth=1)
 ax[1].plot(x_ldot, get_density(low_den_overtime_file),
-           label=r'\rm t = ??? over s', color='black', linestyle='--', linewidth=1)
+           label=time_ldot, color='darkgoldenrod', linestyle='--', linewidth=1)
 ax[1].set_yscale('log')
-ax[1].legend(frameon=False, fontsize=12)
-ax[1].xaxis.set_minor_locator(AutoMinorLocator())
-ax[1].yaxis.set_minor_locator(AutoMinorLocator())
+ax[1].set_xlim(-0.4, 28)
+ax[1].legend(frameon=False, fontsize=12, loc=(0.5, 0.3))
+#ax[1].xaxis.set_minor_locator(AutoMinorLocator())
+#ax[1].yaxis.set_minor_locator(AutoMinorLocator())
 ax[1].tick_params(axis="both", direction="in", which="both", bottom=True, top=True, left=True, right=True, length=2)
+
+
 
 ### Panel - 3 H, He, ionisation profile
 X_H = get_tracer(low_den_overtime_file, 'Tr000_X_H')
@@ -77,17 +86,17 @@ He1p = get_tracer(low_den_overtime_file, "Tr011_He1p")
 He2p = X_He - He0 - He1p
 
 #ax[2].set_xlabel(r'$\rm x \, (cm)$', fontsize=18)
-ax[2].set_ylabel(r'\rm Ionisation fraction', fontsize=12)
-ax[2].plot(x_ldot, H0 / X_H, label="$\mathrm{H}^{0}$")
-ax[2].plot(x_ldot, H1p / X_H, label="$\mathrm{H}^{+}$")
-ax[2].plot(x_ldot, He0 / X_He, label="$\mathrm{He}^{0}$")
-ax[2].plot(x_ldot, He1p / X_He, label="$\mathrm{He}^{+}$")
-ax[2].plot(x_ldot, He2p / X_He, label="$\mathrm{He}^{2+}$")
-ax[2].set_xlim()
+ax[2].set_ylabel(r'\rm Ionisation fraction', fontsize=12,  labelpad=12)
+ax[2].plot(x_ldot, H0 / X_H, label="$\mathrm{H}$", linewidth=1, color='red')
+ax[2].plot(x_ldot, H1p / X_H, label="$\mathrm{H}^{+}$", linewidth=1, color='black')
+ax[2].plot(x_ldot, He0 / X_He, label="$\mathrm{He}$", linewidth=1, linestyle='--', color='turquoise')
+ax[2].plot(x_ldot, He1p / X_He, label="$\mathrm{He}^{+}$", linewidth=1, linestyle='--', color='darkgoldenrod')
+ax[2].plot(x_ldot, He2p / X_He, label="$\mathrm{He}^{2+}$", linewidth=1, linestyle='--', color='magenta')
+ax[2].set_xlim(0, 3)
 ax[2].set_ylim()
-ax[2].legend(frameon=False, fontsize=12, ncol=2)
-ax[2].xaxis.set_minor_locator(AutoMinorLocator())
-ax[2].yaxis.set_minor_locator(AutoMinorLocator())
+ax[2].legend(frameon=False, fontsize=10, ncol=2)
+#ax[2].xaxis.set_minor_locator(AutoMinorLocator())
+#ax[2].yaxis.set_minor_locator(AutoMinorLocator())
 ax[2].tick_params(axis="both", direction="in", which="both", bottom=True, top=True, left=True, right=True, length=2)
 
 ### Panel - 4, Carbon ionisation profile
@@ -102,36 +111,37 @@ C5p = get_tracer(low_den_overtime_file, "Tr017_C5p")
 C6p = X_C - C0 - C1p - C2p - C3p - C4p - C5p
 
 #ax[3].set_xlabel(r'$\rm x \, (cm)$', fontsize=18)
-ax[3].set_ylabel(r'\rm Ionisation fraction', fontsize=12)
-ax[3].plot(x_ldot, C0 / X_C, label="$\mathrm{C}$")
-ax[3].plot(x_ldot, C1p / X_C, label="$\mathrm{C}^{+}$")
-ax[3].plot(x_ldot, C2p / X_C, label="$\mathrm{C}^{2+}$")
-ax[3].plot(x_ldot, C3p / X_C, label="$\mathrm{C}^{3+}$")
-ax[3].plot(x_ldot, C4p / X_C, label="$\mathrm{C}^{4+}$")
-ax[3].plot(x_ldot, C5p / X_C, label="$\mathrm{C}^{5+}$")
-ax[3].plot(x_ldot, C6p / X_C, label="$\mathrm{C}^{6+}$")
-ax[3].set_xlim()
+ax[3].set_ylabel(r'\rm Ionisation fraction', fontsize=12, labelpad=12)
+ax[3].plot(x_ldot, C0 / X_C, label="$\mathrm{C}$", linewidth=1, color='red')
+ax[3].plot(x_ldot, C1p / X_C, label="$\mathrm{C}^{+}$", linewidth=1, linestyle='--', color='black')
+ax[3].plot(x_ldot, C2p / X_C, label="$\mathrm{C}^{2+}$", linewidth=1, color='magenta')
+ax[3].plot(x_ldot, C3p / X_C, label="$\mathrm{C}^{3+}$", linewidth=1, linestyle='--', color='turquoise')
+ax[3].plot(x_ldot, C4p / X_C, label="$\mathrm{C}^{4+}$", linewidth=1, color='saddlebrown', linestyle='--')
+ax[3].plot(x_ldot, C5p / X_C, label="$\mathrm{C}^{5+}$", linewidth=1, color='orange')
+ax[3].plot(x_ldot, C6p / X_C, label="$\mathrm{C}^{6+}$", linewidth=1, color='olivedrab', linestyle='--')
 ax[3].set_ylim()
-ax[3].legend(frameon=False, fontsize=12, ncol=2)
-ax[3].xaxis.set_minor_locator(AutoMinorLocator())
-ax[3].yaxis.set_minor_locator(AutoMinorLocator())
+ax[3].set_xlim(0, 3)
+ax[3].legend(frameon=False, fontsize=10, ncol=2)
+#ax[3].xaxis.set_minor_locator(AutoMinorLocator())
+#ax[3].yaxis.set_minor_locator(AutoMinorLocator())
 ax[3].tick_params(axis="both", direction="in", which="both", bottom=True, top=True, left=True, right=True, length=2)
 
 ### Panel - 5
-ax[4].set_xlabel(r'$\rm x \, (cm)$', fontsize=14)
-ax[4].set_ylabel(r'$\rm log \, T \, (K)$', fontsize=14, labelpad=10)
-ax[4].plot(x_ldst, np.log10(get_temperature(low_den_overtime_file)), color='black',
-           linestyle='--', linewidth=1, label=r'$\rm \rho \sim 10^{-23} (g/cm^3)$')
-ax[4].plot(x_hdot, np.log10(get_temperature(high_den_overtime_file)), color='black',
-           linewidth=1, label=r'$\rm \rho \sim 10^{-22} (g/cm^3)$')
-ax[4].set_xlim()
+ax[4].set_xlabel(r'$\rm x \, (pc)$', fontsize=14)
+ax[4].set_ylabel(r'$\rm log \, T \, (K)$', fontsize=14, labelpad=16)
+ax[4].plot(x_ldst, np.log10(get_temperature(low_den_overtime_file)), color='turquoise',
+           linestyle='--', linewidth=1, label=r'$\rm \rho_1 $')
+ax[4].plot(x_hdot, np.log10(get_temperature(high_den_overtime_file)), color='saddlebrown',
+           linewidth=1, label=r'$\rm \rho_2$')
+ax[4].set_xlim(0, 28)
 ax[4].set_ylim()
-ax[4].legend(frameon=False, fontsize=12)
-ax[4].xaxis.set_minor_locator(AutoMinorLocator())
-ax[4].yaxis.set_minor_locator(AutoMinorLocator())
+ax[4].legend(frameon=False, fontsize=12, loc=(0.6, 0.4))
+#ax[4].xaxis.set_minor_locator(AutoMinorLocator())
+#ax[4].yaxis.set_minor_locator(AutoMinorLocator())
 ax[4].tick_params(axis="both", direction="in", which="both", bottom=True, top=True, left=True, right=True, length=2)
 
 
-fig.subplots_adjust(wspace=0.0)
+#fig.subplots_adjust(wspace=0.0)
+plt.subplots_adjust(hspace=0.05)
 plt.tight_layout()
 plt.savefig(output_dir + 'non-adiabatic_analysis.png')
