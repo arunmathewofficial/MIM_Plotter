0a14c0c97b07f2b6fc34216fd9e5994d75d3b279
Arun Mathew <arun@cp.dias.ie>
2024-07-10 22:01:02 +1000

    updating

--- cx_cie_compare.py
+++ cx_cie_compare.py
@@ -34,7 +34,84 @@ temperature_cx_off = np.log10(temperature_cx_off)
 temperature_cx_on = get_temperature(cx_on_cie_silo)
 temperature_cx_on = np.log10(temperature_cx_on)
 
-fig, axs = plt.subplots(2, 1, figsize=(5, 8))
+fig, axs = plt.subplots(1, 2, figsize=(5, 8))
+
+# NITROGEN ##############################################################
+# y data - neon species cx off ###########
+X_N = get_tracer(cx_off_cie_silo, "Tr005_X_N")
+N0 = get_tracer(cx_off_cie_silo, "Tr033_N")
+N1p = get_tracer(cx_off_cie_silo, "Tr034_N1p")
+N2p = get_tracer(cx_off_cie_silo, "Tr035_N2p")
+N3p = get_tracer(cx_off_cie_silo, "Tr036_N3p")
+N4p = get_tracer(cx_off_cie_silo, "Tr037_N4p")
+
+axs[0].plot(temperature_cx_off, N0 / X_N, label="$\mathrm{Ne}^{0}$", color='black')
+axs[0].plot(temperature_cx_off, N1p / X_N, label="$\mathrm{Ne}^{+}$", color='black')
+axs[0].plot(temperature_cx_off, N2p / X_N, label="$\mathrm{Ne}^{2+}$", color='black')
+axs[0].plot(temperature_cx_off, N3p / X_N, label="$\mathrm{Ne}^{3+}$", color='black')
+axs[0].plot(temperature_cx_off, N4p / X_N, label="$\mathrm{Ne}^{4+}$", color='black')
+
+# y data - neon species cx on ###########
+X_N = get_tracer(cx_on_cie_silo, "Tr005_X_N")
+N0 = get_tracer(cx_on_cie_silo, "Tr033_N")
+N1p = get_tracer(cx_on_cie_silo, "Tr034_N1p")
+N2p = get_tracer(cx_on_cie_silo, "Tr035_N2p")
+N3p = get_tracer(cx_on_cie_silo, "Tr036_N3p")
+N4p = get_tracer(cx_on_cie_silo, "Tr037_N4p")
+
+axs[0].plot(temperature_cx_on, N0 / X_N, linestyle='--', color='black')
+axs[0].plot(temperature_cx_on, N1p / X_N, linestyle='--', color='black')
+axs[0].plot(temperature_cx_on, N2p / X_N, linestyle='--', color='black')
+axs[0].plot(temperature_cx_on, N3p / X_N, linestyle='--', color='black')
+axs[0].plot(temperature_cx_on, N4p / X_N, linestyle='--', color='black')
+
+axs[0].tick_params(labelsize=16)
+axs[0].legend(fontsize=12, loc="lower right")
+axs[0].set_ylabel(r"\rm Ionisation Fraction", fontsize=16)
+axs[0].set_xlabel(r"\rm T (K)", fontsize=16)
+axs[0].set_yscale('log')
+axs[0].set_ylim(10**-2, 1.0)
+axs[0].set_xlim(4.0, 5.2)
+
+
+# OXYGEN ################################################################
+# NITROGEN ##############################################################
+# y data - neon species cx off ###########
+X_O = get_tracer(cx_off_cie_silo, "Tr005_X_O")
+O0 = get_tracer(cx_off_cie_silo, "Tr033_O")
+O1p = get_tracer(cx_off_cie_silo, "Tr034_O1p")
+O2p = get_tracer(cx_off_cie_silo, "Tr035_O2p")
+O3p = get_tracer(cx_off_cie_silo, "Tr036_O3p")
+O4p = get_tracer(cx_off_cie_silo, "Tr037_O4p")
+
+axs[0].plot(temperature_cx_off, O0 / X_O, label="$\mathrm{Ne}^{0}$", color='black')
+axs[0].plot(temperature_cx_off, O1p / X_O, label="$\mathrm{Ne}^{+}$", color='black')
+axs[0].plot(temperature_cx_off, O2p / X_O, label="$\mathrm{Ne}^{2+}$", color='black')
+axs[0].plot(temperature_cx_off, O3p / X_O, label="$\mathrm{Ne}^{3+}$", color='black')
+axs[0].plot(temperature_cx_off, O4p / X_O, label="$\mathrm{Ne}^{4+}$", color='black')
+
+# y data - neon species cx on ###########
+X_O = get_tracer(cx_on_cie_silo, "Tr005_X_O")
+O0 = get_tracer(cx_on_cie_silo, "Tr033_O")
+O1p = get_tracer(cx_on_cie_silo, "Tr034_O1p")
+O2p = get_tracer(cx_on_cie_silo, "Tr035_O2p")
+O3p = get_tracer(cx_on_cie_silo, "Tr036_O3p")
+O4p = get_tracer(cx_on_cie_silo, "Tr037_O4p")
+
+axs[0].plot(temperature_cx_on, O0 / X_O, linestyle='--', color='black')
+axs[0].plot(temperature_cx_on, O1p / X_O, linestyle='--', color='black')
+axs[0].plot(temperature_cx_on, O2p / X_O, linestyle='--', color='black')
+axs[0].plot(temperature_cx_on, O3p / X_O, linestyle='--', color='black')
+axs[0].plot(temperature_cx_on, O4p / X_O, linestyle='--', color='black')
+
+axs[0].tick_params(labelsize=16)
+axs[0].legend(fontsize=12, loc="lower right")
+axs[0].set_ylabel(r"\rm Ionisation Fraction", fontsize=16)
+axs[0].set_xlabel(r"\rm T (K)", fontsize=16)
+axs[0].set_yscale('log')
+axs[0].set_ylim(10**-2, 1.0)
+axs[0].set_xlim(4.0, 5.2)
+
 # NEON ###################################################################
 # y data - neon species cx off ###########
 X_Ne = get_tracer(cx_off_cie_silo, "Tr005_X_Ne")
@@ -76,17 +153,17 @@ Ne8p = get_tracer(cx_on_cie_silo, "Tr041_Ne8p")
 Ne9p = get_tracer(cx_on_cie_silo, "Tr042_Ne9p")
 Ne10p = X_Ne - (Ne0 + Ne1p + Ne2p + Ne3p + Ne4p + Ne5p + Ne6p + Ne7p + Ne8p + Ne9p)
 
-axs[0].plot(temperature_cx_off, Ne0 / X_Ne, linestyle='--', color='black')
-axs[0].plot(temperature_cx_off, Ne1p / X_Ne, linestyle='--', color='black')
-axs[0].plot(temperature_cx_off, Ne2p / X_Ne, linestyle='--', color='black')
-#axs[0].plot(temperature_cx_off, Ne3p / X_Ne, linestyle='--', color='black')
-#axs[0].plot(temperature_cx_off, Ne4p / X_Ne, linestyle='--', color='black')
-#axs[0].plot(temperature_cx_off, Ne5p / X_Ne, linestyle='--', color='black')
-#axs[0].plot(temperature_cx_off, Ne6p / X_Ne, linestyle='--', color='black')
-#axs[0].plot(temperature_cx_off, Ne7p / X_Ne, linestyle='--', color='black')
-#axs[0].plot(temperature_cx_off, Ne8p / X_Ne, linestyle='--', color='black')
-#axs[0].plot(temperature_cx_off, Ne9p / X_Ne, linestyle='--', color='black')
-#axs[0].plot(temperature_cx_off, Ne10p / X_Ne, linestyle='--', color='black')
+axs[0].plot(temperature_cx_on, Ne0 / X_Ne, linestyle='--', color='black')
+axs[0].plot(temperature_cx_on, Ne1p / X_Ne, linestyle='--', color='black')
+axs[0].plot(temperature_cx_on, Ne2p / X_Ne, linestyle='--', color='black')
+#axs[0].plot(temperature_cx_on, Ne3p / X_Ne, linestyle='--', color='black')
+#axs[0].plot(temperature_cx_on, Ne4p / X_Ne, linestyle='--', color='black')
+#axs[0].plot(temperature_cx_on, Ne5p / X_Ne, linestyle='--', color='black')
+#axs[0].plot(temperature_cx_on, Ne6p / X_Ne, linestyle='--', color='black')
+#axs[0].plot(temperature_cx_on, Ne7p / X_Ne, linestyle='--', color='black')
+#axs[0].plot(temperature_cx_on, Ne8p / X_Ne, linestyle='--', color='black')
+#axs[0].plot(temperature_cx_on, Ne9p / X_Ne, linestyle='--', color='black')
+#axs[0].plot(temperature_cx_on, Ne10p / X_Ne, linestyle='--', color='black')
 
 axs[0].tick_params(labelsize=16)
 axs[0].legend(fontsize=12, loc="lower right")
